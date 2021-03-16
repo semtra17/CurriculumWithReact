1b751674f7b545ab305aa0911cacd24ced38a57e
Luciano Assefh <semtra17@gmail.com>
2021-03-16 20:38:32 -0300

    Cambio hover contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -15,9 +15,8 @@ export default class Contact extends Component{
                         <div className="imgBx">
                             <img alt="wp" src="../whatsapp.png"></img>
                         </div>
-                        <div className="layer layer1"></div>
-                        <div className="layer layer2"></div>
-                        <div className="contentBx">
+                        
+                        <div className="content">
                             <div>
                                 <h2>WhatsApp</h2>
                                 <p>(0385) 154749296</p>
@@ -28,9 +27,8 @@ export default class Contact extends Component{
                         <div className="imgBx">
                             <img alt="gmail" src="../gmail.png"></img>
                         </div>
-                        <div className="layer layer1"></div>
-                        <div className="layer layer2"></div>
-                        <div className="contentBx">
+                        
+                        <div className="content">
                             <div>
                                 <h2>Gmail</h2>
                                 <p>semtra17@gmail.com</p>
@@ -41,10 +39,8 @@ export default class Contact extends Component{
                     <a  href="/linkedin.com/in/luciano-aizar-assefh-80a09b1b4" className="box">
                         <div className="imgBx">
                             <img alt="linkdin" src="../linkedin (1).png"></img>
-                        </div>
-                        <div className="layer layer1"></div>
-                        <div className="layer layer2"></div>
-                        <div className="contentBx">
+                        </div>  
+                        <div className="content">
                             <div>
                                 <h2>LinkedIn</h2>
                                 <p>linkedin.com/in/luciano-aizar-assefh-80a09b1b4</p>
@@ -55,9 +51,8 @@ export default class Contact extends Component{
                         <div className="imgBx">
                             <img alt="telegram" src="../telegram.png"></img>
                         </div>
-                        <div className="layer layer1"></div>
-                        <div className="layer layer2"></div>
-                        <div className="contentBx">
+                        
+                        <div className="content">
                             <div>
                                 <h2>Telegram</h2>
                                 <p>(0385) 154749296</p>
